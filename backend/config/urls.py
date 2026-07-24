174aaafa3c5acf25fdab5a831b1f6015259da18f
vlad <wladyslaw.fedchenko@gmail.com>
2026-07-25 00:01:49 +0200

    Enable Django admin route

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -5,6 +6,8 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
+    path("admin/", admin.site.urls),  # ← добавить
+
     path("api/", include("apps.games.urls")),
     path("api/orders/", include("apps.orders.urls")),
     path(
